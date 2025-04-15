72e1ce066efbbad566e87239b08b08374c681ae9
SanikaSavalajkar <114510422+SanikaSavalajkar@users.noreply.github.com>
2025-04-15 11:24:02 +0530

    Create couriermanagement-db.sql

---
+++ CourierManagementSystem/couriermanagement-db.sql
@@ -0,0 +1,82 @@
+create database couriermanagement; 
+
+use couriermanagement; 
+
+  
+
+create table User (UserID INT PRIMARY KEY, 
+
+Name VARCHAR(255), 
+
+Email VARCHAR(255) UNIQUE,Password VARCHAR(255), 
+
+ContactNumber VARCHAR(20), 
+
+Address TEXT 
+
+); 
+
+  
+
+create table courier (CourierID INT PRIMARY KEY, 
+
+SenderName VARCHAR(255), 
+
+SenderAddress TEXT, 
+
+ReceiverName VARCHAR(255), 
+
+ReceiverAddress TEXT, 
+
+Weight DECIMAL(5, 2), 
+
+Status VARCHAR(50), 
+
+TrackingNumber VARCHAR(20) UNIQUE, 
+
+DeliveryDate DATE); 
+
+  
+
+create table CourierServices (ServiceID INT PRIMARY KEY, 
+
+ServiceName VARCHAR(100), 
+
+Cost DECIMAL(8, 2)); 
+
+  
+
+create table Employee (EmployeeID INT PRIMARY KEY, 
+
+Name VARCHAR(255), 
+
+Email VARCHAR(255) UNIQUE, 
+
+ContactNumber VARCHAR(20), 
+
+Role VARCHAR(50), 
+Salary DECIMAL(10, 2)); 
+
+  
+
+create table Location (LocationID INT PRIMARY KEY, 
+
+LocationName VARCHAR(100), 
+
+Address TEXT ); 
+
+  
+
+create table Payment (PaymentID INT PRIMARY KEY, 
+
+CourierID INT, 
+
+LocationID INT, 
+
+Amount DECIMAL(10, 2), 
+
+PaymentDate DATE, 
+
+FOREIGN KEY (CourierID) REFERENCES Courier(CourierID), 
+
+FOREIGN KEY (LocationID) REFERENCES Location(LocationID)); 
